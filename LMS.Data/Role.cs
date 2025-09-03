e369cc1185f018c60d51ce7382717cd95201906a
UhtredRegression <dominhtriet47@gmail.com>
2025-09-03 14:08:21 +0700

    feat(db): add Role entity to database

---
+++ LMS.Data/Role.cs
@@ -0,0 +1,7 @@
+public class Role
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
+}
