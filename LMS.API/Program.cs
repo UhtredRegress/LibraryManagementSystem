ca8e87121394b54be2355842ad7dc610dbf87532
UhtredRegression <dominhtriet47@gmail.com>
2025-09-04 16:04:48 +0700

    feat: add required depedency for CQRS and FluenValidation

--- LMS.API/Program.cs
+++ LMS.API/Program.cs
@@ -1,4 +1,9 @@
-using LMS.Data;
+using FluentValidation;
+using LMS.Business;
+using LMS.Infrastructure;
+using LMS.Business.Commands;
+using LMS.Infrastructure.Interface;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,14 +13,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer(); // Needed for minimal APIs too
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<AddRoleCommand>());
+builder.Services.AddValidatorsFromAssemblyContaining<AddRoleCommandValidator>();
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+
 
 var app = builder.Build();
-
+        
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // Swagger at root: https://localhost:5001/
+    });
 }
 
 app.UseHttpsRedirection();
