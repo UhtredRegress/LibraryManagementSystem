ff66c8b184986b8564522974036ef4beaf4aa841
UhtredRegression <dominhtriet47@gmail.com>
2025-09-04 16:08:16 +0700

    feat: add separated validator to validate data in application layer

---
+++ LMS.Business/Validator/UserDTOValidator.cs
@@ -0,0 +1,13 @@
+using FluentValidation;
+using LMS.Shared.DTOs;
+
+namespace LMS.Business.Validator;
+
+public class UserDTOValidator : AbstractValidator<UserDTO>
+{
+    public UserDTOValidator()
+    {
+        
+    }
+
+}
