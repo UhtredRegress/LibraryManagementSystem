96274189cb4f0cf4ba3654673d0ed915a7cd517d
UhtredRegression <dominhtriet47@gmail.com>
2025-09-04 16:07:32 +0700

    feat: add API entry point for Role table

---
+++ LMS.API/RoleController.cs
@@ -0,0 +1,34 @@
+using LMS.Business.Commands;
+using LMS.Shared.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API;
+
+[ApiController]
+[Route("api/roles")]
+public class RoleController:ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public RoleController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddRole(RoleDTO roleDTO)
+    {
+        var command = new AddRoleCommand(roleDTO);
+
+        try
+        {
+            var resultRole = await _mediator.Send(command);
+            return Ok(resultRole);
+        }
+        catch (FluentValidation.ValidationException ex)
+        {
+            return BadRequest(ex.Message); 
+        }
+    }
+}
