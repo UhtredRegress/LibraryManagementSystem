25b114088d3bde38f5bee9e23eb8f8f679b8aaeb
UhtredRegression <dominhtriet47@gmail.com>
2025-09-05 09:01:31 +0700

    fix: change redundant behavior of validation for `AddRoleCommand`

--- LMS.Business/Validator/AddRoleCommandValidator.cs
+++ LMS.Business/Validator/AddRoleCommandValidator.cs
@@ -9,6 +9,8 @@ public class AddRoleCommandValidator : AbstractValidator<AddRoleCommand>
 {
     public AddRoleCommandValidator(IRoleRepository repo)
     {
-        RuleFor(x => x.RoleDTO).SetValidator(new RoleDTOValidator(repo));
+        RuleFor(x => x.RoleDTO.Title).NotEmpty().WithMessage("Title is required")
+            .MustAsync(async (title, _ ) => await repo.GetRoleByTitleAsync(title) == null)
+            .WithMessage("Title is already existed");
     }
 }
