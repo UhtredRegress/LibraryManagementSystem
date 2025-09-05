d5d6d6d9429c62b8c8a5ba0399ab832f3cd26c84
UhtredRegression <dominhtriet47@gmail.com>
2025-09-05 16:24:31 +0700

    feat: add `Book` model to database

---
+++ LMS.Domain/Model/Book.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using LMS.Domain.Enum;
+
+namespace LMS.Domain.Model;
+
+public class Book
+{
+    [Column("id")]
+    public int Id { get; set; }
+    [Required]
+    [Column("title")]
+    public string Title { get; set; }
+    [Column("author")]
+    public string? Author { get; set; }
+    [Required]
+    [Column("availabily")]
+    public Availability Availability { get; set; }
+    public string Publisher { get; set; }
+    [Column("description")]
+    public string? Description { get; set; }
+    [Column("published_date")]
+    public DateTime? PublishDate { get; set; }
+    [Required]
+    [Column("create_at")]
+    public DateTime CreatedAt { get; set; }
+    [Required]
+    [Column("modified_at")]
+    public DateTime ModifiedAt { get; set; }
+    
+}
